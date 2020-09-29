b5de0b789a22e74c34ff9728273b1ca02166a6e6
Vikram Mishra <Vikram.Mishra@honeywell.com>
2020-09-29 11:40:17 +0530

    Addition of a new file to cube code

---
+++ roll_cube_modified.py
@@ -0,0 +1,12 @@
+import random
+def random_num(val2):
+	min=1
+	max=val2
+	return(random.randint(min,max))
+response =input("\tDo you want to Roll a dice!! \n\n \t Enter response:\t")
+while (response == 'y') or (response =='yes'):
+	num=int(input("\nEnter the no of faces in a dice again:\t"))
+	face = random_num(num)
+	print("\n\n\t\t_________")
+	print("\t\t|-------|\n\t\t|  ",face,"\t|\n\t\t|_______|")
+	response = input("\n\n Do u want to roll it again? \n \n Enter response:\t")
